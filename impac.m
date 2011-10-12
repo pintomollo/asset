edcac03f6623aefd64e544d6adf86faf896835bb
blanchou <blanchou@4d5156cf-6c31-4c50-bb28-7d244e0b5ca5>
2011-10-12 12:18:12 +0000

    Should not have changed impac...

--- impac.m
+++ impac.m
@@ -41,9 +41,9 @@ function [pac, hits] = impac(contour, nsteps, thresh, method)
     % Rough incircle radius estimation (from the bounding box)
     thresh = 0.1 * min((max(contour, [], 1) - min(contour, [], 1))) / 2.5;
 
-    %if (method(1) == 'i')
+    if (method(1) == 'i')
       thresh = thresh / 4;
-    %end
+    end
   end
 
   if (isempty(nsteps))
