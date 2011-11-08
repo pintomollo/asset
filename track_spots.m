a80c52a5e42a18736f405ed08db298db842f030e
blanchou <blanchou@4d5156cf-6c31-4c50-bb28-7d244e0b5ca5>
2011-11-08 13:54:48 +0000

    Corrected tracking based on Jaqaman's comments

--- track_spots.m
+++ track_spots.m
@@ -149,7 +149,7 @@ function links = track_spots(spots, opts)
   nends = size(ends, 1);
   ninterm = size(interm, 1);
 
-  dist = Inf(nstarts + nends + ninterm);
+  dist = Inf(nstarts + nends + 2*ninterm);
 
   closing_func = opts.spot_tracking.gap_function;
 
@@ -163,35 +163,36 @@ function links = track_spots(spots, opts)
   alt_dist = eye(max(nends, nstarts))*alt_cost;
   alt_dist(alt_dist==0) = Inf;
 
-  trans_dist = (mutual_dist.' < Inf) * min(mutual_dist(:));
-  trans_dist(trans_dist == 0) = Inf;
-
   [merge_dist, merge_weight, alt_weight] = joining_weight(ends, interm, spots, links);
   frame_indx = -bsxfun(@minus, ends(:,end), interm(:,end).');
   merge_weight = merge_dist .* merge_weight;
   merge_weight(merge_dist > spot_max_movement | frame_indx ~= 1) = Inf;
   alt_weight = avg_movement * alt_weight;
-  alt_weight = repmat(alt_weight, [ceil(nends / ninterm) 1]);
+  %alt_weight = repmat(alt_weight, [ceil(nends / ninterm) 1]);
 
   [split_dist, split_weight, alt_split_weight] = splitting_weight(interm, starts, spots, links);
   frame_indx = -bsxfun(@minus, interm(:,end), starts(:,end).');
   split_weight = split_dist .* split_weight;
   split_weight(split_dist > spot_max_movement | frame_indx ~= 1) = Inf;
   alt_split_weight = avg_movement * alt_split_weight;
-  alt_split_weight = repmat(alt_split_weight, [1, ceil(nstarts / ninterm)]);
+  %alt_split_weight = repmat(alt_split_weight, [1, ceil(nstarts / ninterm)]);
 
   % Note that end-end merging and start-start splitting is not allowed by this
   % algorithm, which might make sense...
 
   dist(1:nends,1:nstarts) = mutual_dist;
   dist(1:nends,nstarts+1:nstarts+ninterm) = merge_weight;
-  dist(1:nends,nstarts+ninterm+1:end) = alt_dist(1:nends,1:nends);
-
   dist(nends+1:nends+ninterm,1:nstarts) = split_weight;
-  dist(nends+1:nends+ninterm,nstarts+ninterm+1:end) = alt_split_weight(:, 1:nends);
 
-  dist(nends+ninterm+1:end,1:nstarts) = alt_dist(1:nstarts,1:nstarts);
-  dist(nends+ninterm+1:end,nstarts+1:nstarts+ninterm) = alt_weight(1:nstarts, :);
+  dist(1:nends,nstarts+ninterm+1:end-ninterm) = alt_dist(1:nends,1:nends);
+  dist(nends+ninterm+1:end-ninterm,1:nstarts) = alt_dist(1:nstarts,1:nstarts);
+
+  dist(nends+1:nends+ninterm,end-ninterm+1:end) = alt_split_weight;
+  dist(end-ninterm+1:end,nstarts+1:nstarts+ninterm) = alt_weight;
+
+  trans_dist = (dist(1:nends+ninterm, 1:nstarts+ninterm).' < Inf) * min(mutual_dist(:));
+  trans_dist(trans_dist == 0) = Inf;
+
   dist(nends+ninterm+1:end,nstarts+ninterm+1:end) = trans_dist;
 
   disp('Let''s go !')
