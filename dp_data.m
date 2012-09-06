a16d335b078da1231e36051fb90d59bdb7a27b9a
blanchou <blanchou@4d5156cf-6c31-4c50-bb28-7d244e0b5ca5>
2012-09-06 11:17:56 +0000

    Implement the segmentation using only the data channel and collected the domain expansion parameters into one file

---
+++ dp_data.m
@@ -0,0 +1,248 @@
+function mymovie = dp_data(mymovie, nimg, opts)
+
+  if (isfield(mymovie, 'data'))
+    if (isfield(mymovie.data, 'centers') && ~isempty(mymovie.data.centers))
+      centers = mymovie.data.centers;
+      axes_length = mymovie.data.axes_length;
+      orientations = mymovie.data.orientations;
+      eggshell = mymovie.data.eggshell;
+      cortex = mymovie.data.cortex;
+
+      if (~isfield(mymovie.data, 'update'))
+        update = false(size(centers));
+      else
+        update = mymovie.data.update;
+      end
+
+      if (~isfield(mymovie.data, 'ruffles'))
+        ruffles = get_struct('ruffles', size(eggshell));
+      else
+        ruffles = mymovie.data.ruffles;
+      end
+    else
+      [nframes, imgsize] = size_data(mymovie.data);
+
+      centers = NaN(2,nframes);
+      axes_length = NaN(2,nframes);
+      orientations = NaN(1,nframes);
+
+      update = false(2,nframes);
+
+      eggshell = get_struct('eggshell',[1,nframes]);
+      cortex = get_struct('cortex',[1,nframes]);
+      ruffles = get_struct('ruffles',[1, nframes]);
+      
+      if (~isfield(mymovie.data, 'neighbors') | isempty(mymovie.data.neighbors))
+        mymovie = split_cells(mymovie, opts);
+      end
+    end
+  else
+    error 'No suitable channels available for the data segmentation'; 
+
+    return;
+  end
+
+  neighbors = mymovie.data.neighbors;
+  parameters = opts.segmentation_parameters.data;
+
+  img = [];
+
+  if (length(cortex) < nimg | isempty(cortex(nimg).carth) | opts.recompute | (~strncmp(opts.do_ml, 'none', 4) & (strncmp(opts.ml_type, 'cortex', 6) | strncmp(opts.ml_type, 'all', 3))))
+
+    update(2, nimg) = true;
+    [centers(:,nimg), axes_length(:,nimg), orientations(1,nimg), neighbors(nimg)] = detect_ellipse(neighbors(nimg), opts);
+    img = imnorm(double(load_data(mymovie.data, nimg)));
+    img = mask_neighbors(img, centers(:,nimg), axes_length(:,nimg), orientations(1,nimg), neighbors(nimg), opts);
+      %img = mask_neighbors(img, centers(:,nimg), axes_length(:,nimg), orientations(1,nimg), neighbors(nimg), opts);
+
+    if (opts.verbosity == 3)
+      figure;imshow(realign(img,[388 591],centers(:,nimg),orientations(1,nimg)));
+    end
+  
+
+    %polar_img = elliptic_coordinate(img, centers(:,nimg), axes_length(:,nimg), orientations(1,nimg), parameters.safety);
+
+    %figure;imagesc(polar_img);
+
+    %polar_img = imnorm(polar_img,[],[],'rows');
+
+  
+    %figure;imagesc(polar_img);
+
+    %mask = roipoly(size(img,1),size(img,2), eggshell(nimg).carth(:,1), eggshell(nimg).carth(:,2));
+
+    %noise = img;
+    %noise(mask) = -1;
+    %img = img .* mask;
+    
+    %minimg = mean(mean(noise(noise~=-1)));
+    %img = imnorm(img, minimg, []);
+
+    img = gaussian_mex(img, parameters.noise.gaussian);
+    img = median_mex(img, parameters.noise.median);
+    %img = imfilter(img,parameters.noise.gaussian,'symmetric');
+    %img = medfilt2(img,parameters.noise.median);
+    img = imnorm(img);
+
+    polar_img = elliptic_coordinate(img, centers(:,nimg), axes_length(:,nimg), orientations(1,nimg), parameters.safety);
+    
+    %figure;imagesc(polar_img);
+    polar_img = imnorm(polar_img,[],[],'rows');
+
+    polar_size = size(polar_img);
+
+    %egg_path = carth2elliptic(eggshell(nimg).carth, centers(:,nimg), axes_length(:,nimg), orientations(1,nimg));
+    %egg_path = elliptic2pixels(egg_path, polar_size, axes_length(:,nimg), parameters.safety);
+
+    %egg_path = adapt_path(polar_size, egg_path);
+
+    %egg_path = adapt_path(size(polar_img), parameters.safety, ellpts);
+
+    %parameters.cortex_weights.path = egg_path;
+
+    if (opts.compute_probabilities)
+      [cortex_path, emissions, transitions] = dynamic_programming(polar_img, parameters.cortex_params, parameters.scoring_func{2}, parameters.cortex_weights, opts);
+      [cortex_path, emissions] = remove_polar_body(polar_img, cortex_path, parameters.cortex_params, parameters.scoring_func{2}, parameters.cortex_weights, opts, emissions);
+
+      %'SET POWERS !!'
+      [beta, gamma, probs] = find_temperatures(transitions, emissions, opts.temperatures);
+      cortex(nimg).temperatures = [beta; gamma];
+      %[entropy] = posterior_decoding(cortex_path, emissions, transitions, 1, 1);
+    else
+      parameters.cortex_params.nhood = 11;
+      parameters.cortex_params.alpha = 0.4;
+      parameters.cortex_params.beta = 0.85;
+      parameters.cortex_params.gamma = 0.1;
+
+      parameters.cortex_weights.alpha = 0.55;
+      parameters.cortex_weights.beta = 0.05;
+      parameters.cortex_weights.gamma = 0.45;
+
+      %print_all(parameters)
+      %figure;imagesc(log(parameters.scoring_func{2}(polar_img, parameters.cortex_weights)));
+
+      cortex_path = dynamic_programming(polar_img, parameters.cortex_params, parameters.scoring_func{2}, parameters.cortex_weights, opts);
+      cortex_path = remove_polar_body(polar_img, cortex_path, parameters.cortex_params, parameters.scoring_func{2}, parameters.cortex_weights, opts);
+
+      %figure;imshow(polar_img);
+      %hold on;plot(cortex_path,[1:length(cortex_path)],'Color',[1 0.5 0]);
+
+      %keyboard
+    end
+
+    ellpts = pixels2elliptic(cortex_path,size(polar_img), axes_length(:,nimg),parameters.safety);
+    carths = elliptic2carth(ellpts,centers(:,nimg),axes_length(:,nimg),orientations(1,nimg));
+    carths = carths(all(carths >= 1 & bsxfun(@le, carths , fliplr(size(img))), 2), :);
+
+    if (opts.measure_performances)
+      [estimation] = detect_ellipse(img, opts);
+    end
+
+    if (opts.verbosity == 3)
+      figure;imshow(polar_img);
+      hold on;plot(cortex_path,[1:length(cortex_path)],'Color',[1 0.5 0]);
+      %plot(egg_path,[1:length(cortex_path)],'g');
+
+      figure;
+      imshow(realign(img,[388 591],centers(:,nimg),orientations(1,nimg)));
+      hold on;
+      myplot(realign(carths,[388 591],centers(:,nimg),orientations(1,nimg)),'Color',[1 0.5 0]);
+      %myplot(realign(eggshell(nimg).carth,[388 591],centers(:,nimg),orientations(1,nimg)),'g');
+      myplot(realign(draw_ellipse(centers(:,nimg), axes_length(:,nimg), orientations(1,nimg)),[388 591],centers(:,nimg), orientations(1,nimg)),'m');
+
+    end
+
+    %cortex(nimg).raw = cortex_path;
+    %cortex(nimg).elliptic = ellpts;
+    cortex(nimg).carth = carths;
+
+    if (opts.measure_performances)
+      cortex(nimg).estim = estimation;
+    end
+
+    mymovie.data.centers = centers;
+    mymovie.data.axes_length = axes_length;
+    mymovie.data.orientations = orientations;
+    mymovie.data.cortex = cortex;
+    mymovie.data.ruffles(nimg) = get_struct('ruffles');
+  end
+
+  if (length(eggshell) < nimg | isempty(eggshell(nimg).carth) | opts.recompute | (~strncmp(opts.do_ml, 'none', 4) & (strncmp(opts.ml_type, 'eggshell', 8) | strncmp(opts.ml_type, 'all', 3))))
+
+    update(1, nimg) = true;
+
+    parameters.eggshell_weights.alpha = 0.02;
+
+    path = cortex(nimg).carth;
+    indxs = convhull(path(:,1), path(:,2));
+    path = path(indxs, :);
+
+    [centers(:,nimg), axes_length(:,nimg), orientations(1,nimg)] = fit_ellipse(carths);
+    egg_path = carth2elliptic(path, centers(:,nimg), axes_length(:,nimg), orientations(1,nimg), 'radial');
+
+    max_rad = max(egg_path(:, 2)) * (1+parameters.eggshell_weights.alpha);
+    axes_length(:, nimg) = axes_length(:, nimg) * max_rad;
+
+    eggshell(nimg).carth = draw_ellipse(centers(:,nimg), axes_length(:,nimg), orientations(1,nimg));
+
+
+    mymovie.data.centers = centers;
+    mymovie.data.axes_length = axes_length;
+    mymovie.data.orientations = orientations;
+    mymovie.data.eggshell = eggshell;
+  end
+
+
+  mymovie.data.parameters = parameters;
+  mymovie.data.update = update;
+
+  return;
+end
+
+function [center, axes_length, orientation, neighbors, estim] = detect_ellipse(neighbors, img, opts)
+
+  if (nargin == 2)
+    if (isstruct(neighbors))
+      opts = img;
+      img = [];
+      estim_only = false;
+    else
+      tmp = neighbors;
+      opts = img;
+      img = tmp;
+      neighbors = [];
+      estim_only = true;
+    end
+  else
+    estim_only = false;
+  end
+
+  center = NaN(2, 1);
+  axes_length = NaN(2, 1);
+  orientation = NaN;
+  estim = [];
+
+  if (~isempty(img))
+    [ellipse, estim] =  split_cells(img, true, opts);
+  end
+
+  if (estim_only)
+    center = estim;
+    axes_length = [];
+    orientation = [];
+    estim = [];
+
+    return;
+  end
+
+  if (all(isnan(neighbors.centers(:))) | neighbors.index <= 0)
+    return;
+  end
+  
+  [center, axes_length, orientation] = deal(neighbors.centers(:, neighbors.index), ...
+                                            neighbors.axes_length(:, neighbors.index), ...
+                                            neighbors.orientations(:, neighbors.index));
+
+
+  return;
+end
